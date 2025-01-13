c277783909700d18179651b641ea421b271f7173
Timofey Ivlev <timofejivlev923@gmail.com>
2025-01-14 00:13:23 +0300

    update receiver

--- TaskFlow/TaskFlow/urls.py
+++ TaskFlow/TaskFlow/urls.py
@@ -3,7 +3,7 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from tasks.views import TaskViewSet
+from tasks.views import TaskViewSet, UpdateTaskStatusAPIView
 from tasks.views import CreateUserAPI
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView , TokenVerifyView
 router = DefaultRouter()
@@ -19,4 +19,5 @@ urlpatterns = [
     path('register/', CreateUserAPI.as_view(), name='register'),
     path('api/', include(router.urls)),
     path('login/', TokenObtainPairView.as_view() , name = 'login' ),
+    path('change-status/<int:pk>/', UpdateTaskStatusAPIView.as_view() , name = 'change-status' ),
 ]
--- TaskFlow/tasks/views.py
+++ TaskFlow/tasks/views.py
@@ -3,6 +3,7 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status, viewsets, generics
+from rest_framework.exceptions import PermissionDenied
 
 from .permissions import IsGiver
 from .serializers import RegisterSerializer, TaskSerializer
@@ -22,12 +23,20 @@ class CreateUserAPI(generics.CreateAPIView):
         return response
 
 
+class UpdateTaskStatusAPIView(APIView):
+    def patch(self,request,pk):
+        task = Task.objects.get(pk=pk)
 
+        new_status = request.data.get('status')
+        if new_status in dict(Task.STATUS_CHOICES).keys():
+            task.status = new_status
+            task.save()
+            return Response(TaskSerializer(task).data)
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
-    permission_classes = [IsGiver, IsAuthenticated]
+    permission_classes = [ IsAuthenticated]
     queryset = Task.objects.all()
     def get_queryset(self):
         user = self.request.user  # Получаем текущего пользователя
@@ -42,6 +51,12 @@ class TaskViewSet(viewsets.ModelViewSet):
         return Task.objects.none()  # Если роль не определена, ничего не возвращаем
 
     def perform_create(self, serializer):
+        user = self.request.user  # Получаем текущего пользователя
+
+        # Проверяем, что пользователь имеет роль "giver" и может создавать задачи
+        if user.profile.role != 'giver':
+            raise PermissionDenied("Only users with role 'giver' can create tasks.")
+
         # Сохраняем задачу и назначаем текущего пользователя как giver
         task = serializer.save(giver=self.request.user)
 
